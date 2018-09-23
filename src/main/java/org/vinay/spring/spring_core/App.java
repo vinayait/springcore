26f4609c95837e249289247fec1075088671bcbf
vinay <vinaypandey.ait@gmail.com>
2018-09-23 11:41:42 -0400

    created springmanagedbeans branch

--- src/main/java/org/vinay/spring/spring_core/App.java
+++ src/main/java/org/vinay/spring/spring_core/App.java
@@ -12,7 +12,7 @@ public class App {
 				BeanConfiguration.class);
 		AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
 		
-		System.out.println("\nBefore money trasnfer ");
+		System.out.println("\nBefore money transfer");
 		System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
 		System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
 		
